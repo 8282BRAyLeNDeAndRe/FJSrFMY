6a505e8ca5af69819f01768870cd89ba9783a6c5
8282BRAyLeNDeAndRe <lop-smudgy-0o@icloud.com>
2025-09-22 17:44:32 +0100

    Introduce JSON数据格式转换器

---
+++ json_data_formatter_0923_0044_sku.vue
@@ -0,0 +1,46 @@
+// 代码生成时间: 2025-09-23 00:44:31
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'JsonDataFormatter',
+  setup() {
+    const inputData = ref('');
+    const formattedData = ref('');
+    const error = ref('');
+
+    function formatJSON(): void {
+      try {
+        const parsed = JSON.parse(inputData.value);
+        formattedData.value = JSON.stringify(parsed, null, 2);
+        error.value = '';
+      } catch (e) {
+        error.value = 'Invalid JSON';
+        formattedData.value = '';
+      }
+    }
+
+    return {
+      inputData,
+      formattedData,
+      error,
+      formatJSON
+    };
+  }
+});
+</script>
+
+<style scoped>
+  .error {
+    color: red;
+  }
+</style>
+
+/*
+ * JSON Data Formatter Vue Component
+ * This component allows users to input JSON data,
+ * and then format it into a more readable form.
+ *
+ * @author Your Name
+ * @version 1.0
+ * @license MIT
+ */
